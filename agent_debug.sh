8b66181cc558a5da061af733d9ec5164f204afc0
Gustavo Massaneiro <gustavomassa123@gmail.com>
2021-12-24 16:07:03 -0300

    script to debug tapper running dlv-dap with sudo

---
+++ agent_debug.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DLV=$(which dlv-dap)
+if [ -x "${DLV}" ] ; then
+	PATH="${GOPATH}/bin/:$PATH"
+fi
+
+sudo setcap cap_net_raw,cap_net_admin=eip ./agent/build/mizuagent
+export GOGC=12800 
+export NODE_NAME=dev
+
+# The parameter -C 4 keeps the descriptor 3 opened
+exec sudo -C 4 "$DLV" --only-same-user=false --headless --listen=:2345 --accept-multiclient exec ./agent/build/mizuagent -- \
+        -i any \
+        --tap \
+        --api-server-address ws://localhost:8899/wsTapper & \
+PID1=$! && \
+read -r -d '' _ </dev/tty
+sudo kill -9 $PID1
